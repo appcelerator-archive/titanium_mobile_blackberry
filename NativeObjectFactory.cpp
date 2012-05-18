d0ee9c1b9c557ed4f15a1e1042d342bfecaefda3
unknown <alexg@AlexG.(none)>
2012-05-18 19:01:54 +0400

    TIMOB-8830: BlackBerry: Implement UI.ProgressBar
    Reviewers: JP, DavidC
    
    ChangeLog:
    - Update ProgressBar yaml for blackberry
    - Make ctors private and explicit, disable copy ctors and operator=
    - Make dtors protected
    - Create show, hide methods which doing nothing for now, since progressbar already added to container. Added for consistensy with Titanium API.
    
    Test Cases:
    - Build tibb.
    - Build tibbtest.
    - In the java script, instance progressbar object.
    - Make sure existing properties (max, min, value) work.
    
    - Run docgen.py script
    - Make sure documentation includes appropriates changes for blackberry platform

--- NativeObjectFactory.cpp
+++ NativeObjectFactory.cpp
@@ -61,8 +61,9 @@ NativeObject* NativeObjectFactory::createNativeObject(int type)
         break;
 
     case N_TYPE_PROGRESSBAR:
-        obj = new NativeProgressBarObject;
+        obj = NativeProgressBarObject::createProgressBar();
         break;
+
     case N_TYPE_TEXT_FIELD:
         obj = NativeTextFieldObject::createTextField();
         break;
