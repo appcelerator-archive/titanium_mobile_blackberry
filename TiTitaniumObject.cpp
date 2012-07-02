259cce9667bacc007154e1e207bc72f6e8b2459d
Alex Galstyan <alexanderg@macadamian.com>
2012-07-02 16:45:12 +0400

    TIMOB-9539: BlackBerry: Implement include()
    Reviewers: DavidC, DavidL
    
    ChangeLog:
    - Added include functionality to Ti namespace.
    
    Test Cases:
    - Run tibbtest.
    - Make sure include_test.js included in bar-descriptor.xml
    - Make sure Ti.include(<filename>) works correctly.
    - Use the following app.js snippet:
    
    ////////////////////////////////////////
    :app.js
    var win1 = Titanium.UI.createWindow({
        backgroundColor:'#F00'
    });
    
    var label1 = Ti.UI.createLabel({
    	text:'Hello, world!',
    	color:'green',
    	top: 200
    });
    
    win1.add(label1);
    
    Ti.include('include_test.js');
    
    // open window
    win1.open();
    ////////////////////////////////////////
    :include_test.js
    var mybutton=Ti.UI.createButton
    (
    	{top: 100,title: 'Push Me'}
    );
    
    mybutton.addEventListener
    (
    	'click',
    	function(e)
    	{
    		e.source.title='Pushed!';
    		slider1.backgroundColor='#00F';
    		progress1.backgroundColor='#0F0';
    	}
    );
    
    win1.add(mybutton);

--- TiTitaniumObject.cpp
+++ TiTitaniumObject.cpp
@@ -6,8 +6,12 @@
  */
 
 #include "TiTitaniumObject.h"
-#include "TiUIObject.h"
 #include "TiAPIObject.h"
+#include "TiGenericFunctionObject.h"
+#include "TiLogger.h"
+#include "TiUIObject.h"
+
+#include <fstream>
 
 TiTitaniumObject::TiTitaniumObject()
     : TiObject("Titanium")
@@ -30,6 +34,7 @@ void TiTitaniumObject::onCreateStaticMembers()
 {
     ADD_STATIC_TI_VALUE("buildDate", String::New(__DATE__), this);
     ADD_STATIC_TI_VALUE("version", Number::New(2.0), this);
+    TiGenericFunctionObject::addGenericFunctionToParent(this, "include", this, _include);
     // TODO: remove hard coded version number
     TiUIObject::addObjectToParent(this, objectFactory_);
     TiAPIObject::addObjectToParent(this);
@@ -38,4 +43,50 @@ void TiTitaniumObject::onCreateStaticMembers()
 bool TiTitaniumObject::canAddMembers() const
 {
     return false;
+}
+
+Handle<Value> TiTitaniumObject::_include(void* userContext, TiObject* caller, const Arguments& args)
+{
+    if (args.Length() < 1)
+    {
+        return Undefined();
+    }
+
+    Local<Value> javaScript = args[0];
+    if (!javaScript->IsString())
+    {
+        javaScript = javaScript->ToString();
+    }
+
+    string fileName = "app/native/assets/";
+    fileName += *String::Utf8Value(javaScript);
+
+    ifstream ifs(fileName.c_str());
+
+    if (ifs.bad())
+    {
+        return Undefined();
+    }
+
+    string buffer;
+    getline(ifs, buffer, string::traits_type::to_char_type(string::traits_type::eof()));
+    ifs.close();
+
+    TryCatch tryCatch;
+    Handle<Script> compiledScript = Script::Compile(String::New(buffer.c_str()));
+    if (compiledScript.IsEmpty())
+    {
+        String::Utf8Value error(tryCatch.Exception());
+        TiLogger::getInstance().log(string(*error) + "\n");
+        return Undefined();
+    }
+    Handle<Value>result = compiledScript->Run();
+    if (result.IsEmpty())
+    {
+        String::Utf8Value error(tryCatch.Exception());
+        TiLogger::getInstance().log(string(*error) + "\n");
+        return Undefined();
+    }
+
+    return Undefined();
 }
