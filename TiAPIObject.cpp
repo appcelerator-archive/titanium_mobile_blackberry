c9c55c89035bd10aefb5ebebd2ab4edbef2fb6ff
Alex Galstyan <alexanderg@macadamian.com>
2012-07-03 15:37:24 +0400

    TIMOB-9548: BlackBerry: Implement API elements needed for KitchenSink
    Reviewers: DavidL, Harut
    
    ChangeLog:
    - Implemented info, log, error, warn, debug, methods from Ti.API namespace
    
    Test Cases:
    - Run tibbtest.
    - Use the following app.js snippet to test Ti.API functionality:
    
    ////////////////////////////////////////
    :app.js
    //
    // create root window
    //
    var win1 = Titanium.UI.createWindow({
        backgroundColor:'#F00'
    });
    
    var label1 = Ti.UI.createLabel({
    	textAlign : 'center',
    	text:'Hello, world!',
    	color:'green',
    	top: 200
    });
    
    label1.text=new Date();
    
    win1.add(label1);
    
    var strBoy = new String('boy');
    var strGirl = new String('girl');
    
    Ti.API.log('debug', String.format('%s + %s = LOVE', strBoy, strGirl));
    Ti.API.log('warn', String.format('%s + %s = LOVE', strBoy, strGirl));
    Ti.API.log('undefined', String.format('%s + %s = LOVE', strBoy, strGirl));
    Ti.API.log(String.format('%s + %s = LOVE', strBoy, strGirl));
    Ti.API.debug(String.format('%s + %s = LOVE', strBoy, strGirl));
    Ti.API.info(String.format('%s + %s = LOVE', strBoy, strGirl));
    Ti.API.error(String.format('%s + %s = LOVE', strBoy, strGirl));
    Ti.API.warn(String.format('%s + %s = LOVE', strBoy, strGirl));
    
    // open window
    win1.open();

--- TiAPIObject.cpp
+++ TiAPIObject.cpp
@@ -7,6 +7,19 @@
 
 #include "TiAPIObject.h"
 #include "TiGenericFunctionObject.h"
+#include "TiLogger.h"
+
+typedef enum TiSeverityLevel_
+{
+    Ti_DEBUG,
+    Ti_INFO,
+    Ti_ERROR,
+    Ti_WARN,
+    Ti_TRACE
+} TiSeverityLevel;
+
+// Helper method
+static void toSystemLog(Local<Value> message, const TiSeverityLevel sLevel);
 
 TiAPIObject::TiAPIObject()
     : TiObject("API")
@@ -25,25 +38,146 @@ void TiAPIObject::addObjectToParent(TiObject* parent)
 
 void TiAPIObject::onCreateStaticMembers()
 {
-    TiGenericFunctionObject::addGenericFunctionToParent(this, "debug", this, debug_);
+    TiGenericFunctionObject::addGenericFunctionToParent(this, "debug", this, _debug);
+    TiGenericFunctionObject::addGenericFunctionToParent(this, "info", this, _info);
+    TiGenericFunctionObject::addGenericFunctionToParent(this, "warn", this, _warn);
+    TiGenericFunctionObject::addGenericFunctionToParent(this, "error", this, _error);
+    TiGenericFunctionObject::addGenericFunctionToParent(this, "log", this, _log);
+    TiGenericFunctionObject::addGenericFunctionToParent(this, "timestamp", this, _timestamp);
+    TiGenericFunctionObject::addGenericFunctionToParent(this, "trace", this, _trace);
 }
 
-Handle<Value> TiAPIObject::debug_(void* userContext, TiObject* caller, const Arguments& args)
+Handle<Value> TiAPIObject::_debug(void* userContext, TiObject* caller, const Arguments& args)
 {
-    int i;
-    for (i = 0; i < args.Length(); i++)
+    if (args.Length() < 1)
     {
-        if (args[i]->IsArray())
-        {
-            // TODO: complete
-        }
-        else
-        {
-            Local<String> log = args[i]->ToString();
-            String::Utf8Value logString(log);
-            printf("%s", (const char*)(*logString));
-        }
+        return Undefined();
     }
-    printf("\n");
+
+    // Log message with DEBUG severity-level
+    toSystemLog(args[0], Ti_DEBUG);
     return Undefined();
+}
+
+Handle<Value> TiAPIObject::_info(void* userContext, TiObject* caller, const Arguments& args)
+{
+    if (args.Length() < 1)
+    {
+        return Undefined();
+    }
+
+    // Log message with INFO severity-level
+    toSystemLog(args[0], Ti_INFO);
+    return Undefined();
+}
+
+Handle<Value> TiAPIObject::_warn(void* userContext, TiObject* caller, const Arguments& args)
+{
+    if (args.Length() < 1)
+    {
+        return Undefined();
+    }
+
+    // Log message with WARNING severity-level
+    toSystemLog(args[0], Ti_WARN);
+    return Undefined();
+}
+
+Handle<Value> TiAPIObject::_error(void* userContext, TiObject* caller, const Arguments& args)
+{
+    if (args.Length() < 1)
+    {
+        return Undefined();
+    }
+
+    // Log message with ERROR severity-level
+    toSystemLog(args[0], Ti_ERROR);
+    return Undefined();
+}
+
+Handle<Value> TiAPIObject::_log(void* userContext, TiObject* caller, const Arguments& args)
+{
+    if (args.Length() < 2)
+    {
+        return Undefined();
+    }
+
+    Local<Value> sLevel = args[0];
+    if (!sLevel->IsString())
+    {
+        sLevel = sLevel->ToString();
+    }
+
+    // Check the log level
+    String::Utf8Value v8UtfLevel(sLevel);
+    const char* cLevel = *v8UtfLevel;
+    Local<Value> message = args[1];
+    if (strcmp(cLevel, "debug") == 0)
+    {
+        toSystemLog(message, Ti_DEBUG);
+    }
+    else if (strcmp(cLevel, "error") == 0)
+    {
+        toSystemLog(message, Ti_ERROR);
+    }
+    else if (strcmp(cLevel, "info") == 0)
+    {
+        toSystemLog(message, Ti_INFO);
+    }
+    else if (strcmp(cLevel, "warn") == 0)
+    {
+        toSystemLog(message, Ti_WARN);
+    }
+    else if (strcmp(cLevel, "trace") == 0)
+    {
+        toSystemLog(message, Ti_TRACE);
+    }
+    else
+    {
+        toSystemLog(message, Ti_INFO);
+    }
+
+    return Undefined();
+}
+
+Handle<Value> TiAPIObject::_timestamp(void* userContext, TiObject* caller, const Arguments& args)
+{
+    // TODO: Implement this later
+    return Undefined();
+}
+
+Handle<Value> TiAPIObject::_trace(void* userContext, TiObject* caller, const Arguments& args)
+{
+    // TODO: Implement this later
+    return Undefined();
+}
+
+static void toSystemLog(Local<Value> message, const TiSeverityLevel sLevel)
+{
+    if (!message->IsString())
+    {
+        message = message->ToString();
+    }
+
+    switch (sLevel)
+    {
+    case Ti_DEBUG:
+        TI_DEBUG(*String::Utf8Value(message));
+        break;
+    case Ti_INFO:
+        TI_INFO(*String::Utf8Value(message));
+        break;
+    case Ti_WARN:
+        TI_WARNING(*String::Utf8Value(message));
+        break;
+    case Ti_ERROR:
+        TI_ERROR(*String::Utf8Value(message));
+        break;
+    case Ti_TRACE:
+        // TODO: Implement this
+        break;
+    default:
+        TI_INFO(*String::Utf8Value(message));
+        break;
+    }
 }
