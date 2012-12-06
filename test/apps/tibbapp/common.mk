1a5448dfc9fa23bc2ce8addfcec20f666ecf6bcd
Joshua Roesslein <jroesslein@appcelerator.com>
2012-12-05 21:27:09 -0800

    Remove v8preprocessor.

--- test/apps/tibbapp/common.mk
+++ test/apps/tibbapp/common.mk
@@ -26,7 +26,7 @@ CCFLAGS+=-fstack-protector-all -D_FORTIFY_SOURCE=2 \
 LDFLAGS+=-Wl,-z,relro -Wl,-z,now $(if $(filter g so shared,$(VARIANTS)),,-pie)
 
 # Add your required library names, here
-LIBS+=tibb v8 v8preparser socket bps
+LIBS+=tibb v8 socket bps
 
 # Extra .so required
 LDFLAGS+=-lbbcascades -lbbdevice -lbbsystem -lQtCore -lQtDeclarative -lQtGui -lQtMultimedia -lQtNetwork -lm -lbb
