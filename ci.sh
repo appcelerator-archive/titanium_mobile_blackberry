a6393d92a9c740ebc04ac8386749d11ed0b6cb02
euforic <cs@euforic.co>
2013-01-28 14:11:28 -0800

    add ci automation script

---
+++ ci.sh
@@ -0,0 +1,35 @@
+# Auto fetch and package command
+# $ curl -L https://raw.github.com/appcelerator/titanium_mobile_blackberry/master/ci.sh | sh
+
+# Fetch Titanium BlackBerry Sdk
+git clone --recursive https://github.com/appcelerator/titanium_mobile_blackberry
+
+# Enter repo root directory
+cd titanium_mobile_blackberry
+
+# Determine the current git commit hash
+GIT_VERSION=$(/usr/bin/git log -1 --format="%H")
+
+# Create Time Stamp
+DATE=$(date +"%Y%m%d%H%M")
+
+# Set source for build
+source /Applications/bbndk/bbndk-env.sh
+
+# Build v8 runtime
+cd runtime && make
+
+# Move to sdk build file directory
+cd ../cli/commands
+
+# Create temp package.json to install wrench dep
+echo "{\"name\":\"tmp\",\"version\":\"0.0.1\"}" > package.json
+
+# Install node.js wrench module
+npm install wrench && rm package.json
+
+# Build sdk and Move to build output directory
+./create_sdk && cd ../../build/sdk
+
+# Package SDK
+tar -zcvf blackberry_$DATE.tar.gz blackberry
