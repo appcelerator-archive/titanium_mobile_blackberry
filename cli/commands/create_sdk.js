1bf42b19b543512f24358dc8f8bb54202cec382b
Russ McMahon <rmcmahon@appcelerator.com>
2013-04-03 22:59:39 -0700

    remove sdk_version from dropin create script

--- cli/commands/create_sdk.js
+++ cli/commands/create_sdk.js
@@ -26,9 +26,7 @@ copyFileSync('src/tibb/x86/a-g/libtibb.a',  'build/sdk/blackberry/tibb/x86/a-g/l
 copyFileSync('runtime/v8/arm/libv8.a', 'build/sdk/blackberry/libv8/lib/arm/libv8.a');
 copyFileSync('runtime/v8/x86/libv8.a', 'build/sdk/blackberry/libv8/lib/x86/libv8.a');
 
-// bump version before publishing release notes, this way a platform can be have a version and release cycle
-// separate from titanium sdk
-copyFileSync('sdk_version.json', 'build/sdk/blackberry/sdk_version.json');
+
 copyFileSync('package.json', 'build/sdk/blackberry/package.json');
 
 process.chdir(currDir);
