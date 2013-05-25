9d400a45e64d352118a01b7ec0bbfa13c4d181bc
Russ McMahon <rmcmahon@appcelerator.com>
2013-05-24 19:00:17 -0700

    added support for width calculated from left, right, top, bottom property

--- test/apps/native/tibbtest/Resources/app.js
+++ test/apps/native/tibbtest/Resources/app.js
@@ -1,25 +1,22 @@
 var win = Titanium.UI.createWindow();
- 
 
 var scroll = Ti.UI.createScrollView({
-   backgroundColor: '#ccc',
-   width: Ti.UI.FILL,
-   height: Ti.UI.FILL,
-   layout: 'vertical',
-      
+ backgroundColor:'#ccc',
+ width: Ti.UI.FILL,
+ height: Ti.UI.FILL,
+ layout: 'vertical',
 });
 
-
 function MyButton(title) {
-   return Ti.UI.createButton({
-       title: title,
-       top: 10
-   });
+ return Ti.UI.createButton({
+ title: title,
+ top: 10
+ });
 }
-  
-for(var i = 0; i < 500; i++) {
-   scroll.add(MyButton('Button #' + (i + 1))); 
+
+for(var i = 0; i < 100; i++) {
+scroll.add(MyButton('Button #' + (i + 1))); 
 }
-  
+
 win.add(scroll);
 win.open();
