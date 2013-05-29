07928804c50daa505842fe9bfc4a214fb34fa95e
Russ McMahon <rmcmahon@appcelerator.com>
2013-05-29 13:03:43 -0700

    fixed build error not finding defaultWidth_ and defaultHeight_
    Conflicts:
    	test/apps/native/tibbtest/Resources/app.js

--- test/apps/native/tibbtest/Resources/app.js
+++ test/apps/native/tibbtest/Resources/app.js
@@ -1,30 +1,74 @@
-var win = Ti.UI.createWindow({layout:'vertical'});
- 
-var label = Ti.UI.createLabel({
-    text: 'Type some stuff...',
-    width: '75%', height: 50
-});
-win.add(label);
- 
-var textField = Ti.UI.createTextField({
-    width: '75%', height: 75
-});
-win.add(textField);
- 
-var button = Ti.UI.createButton({
-    title: 'Click, me!',
-    width: '75%', height: 100
-});
-win.add(button);
+var win = Ti.UI.createWindow();
 
-button.addEventListener('click',function(e){
-	Ti.API.info('Sending feature event');
-	Titanium.Analytics.featureEvent('my.feature.blah');
+var views = [];
+for(var i = 0; i < 11; i++) {
+	var view = Ti.UI.createView({
+		backgroundColor: '#'+((1<<24)*(Math.random()+1)|0).toString(16).substr(1)
+	});
+	var img = Ti.UI.createView({
+		backgroundImage: 'images/hang_gliding/photo_' + (i + 1) + '.jpg'
+	});
+	view.add(img);
+	views.push(view);
+}
+//
+var otherView = Ti.UI.createView({
+	backgroundColor: 'red'
+});;
+
+views.push(otherView);
+
+var scrollableView = Ti.UI.createScrollableView({
+	currentPage: 4,
+	views: views,
+	top: 0,
+	width: 200,
+	height: 112
 });
- 
-var view = Ti.UI.createView({
-    backgroundColor: 'red'
+
+var scrollToAnimated = Ti.UI.createButton({
+	title: 'scroll animated',
+	bottom: 40,
+	right: 0
 });
-win.add(view);
- 
+
+var scrollToNotAnimated = Ti.UI.createButton({
+	title: 'scroll',
+	bottom: 40,
+	left: 0
+});
+
+var prev = Ti.UI.createButton({
+	bottom: 0,
+	left:0,
+	title: 'prev'
+});
+var next = Ti.UI.createButton({
+	bottom: 0,
+	right:0,
+	title: 'next'
+});
+var x = 0;
+
+prev.addEventListener('click', function(){
+	scrollableView.movePrevious();
+});
+next.addEventListener('click', function(){
+	scrollableView.moveNext();
+});
+
+scrollToAnimated.addEventListener('click', function(){
+	scrollableView.scrollToView(otherView, true);
+});
+
+scrollToNotAnimated.addEventListener('click', function(){
+	scrollableView.scrollToView(2, false);
+});
+win.add(prev);
+win.add(next);
+win.add(scrollToAnimated);
+win.add(scrollToNotAnimated);
+win.add(scrollableView);
+
 win.open();
+
