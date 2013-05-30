21553facac205ffb33f428c9272e99a69bc88645
Russ McMahon <rmcmahon@appcelerator.com>
2013-05-30 08:12:22 -0700

    fixed sized controls to handle margins

--- test/apps/native/tibbtest/Resources/app.js
+++ test/apps/native/tibbtest/Resources/app.js
@@ -1,74 +1,21 @@
 var win = Ti.UI.createWindow();
-
-var views = [];
-for(var i = 0; i < 11; i++) {
-	var view = Ti.UI.createView({
-		backgroundColor: '#'+((1<<24)*(Math.random()+1)|0).toString(16).substr(1)
-	});
-	var img = Ti.UI.createView({
-		backgroundImage: 'images/hang_gliding/photo_' + (i + 1) + '.jpg'
-	});
-	view.add(img);
-	views.push(view);
-}
-//
-var otherView = Ti.UI.createView({
-	backgroundColor: 'red'
-});;
-
-views.push(otherView);
-
-var scrollableView = Ti.UI.createScrollableView({
-	currentPage: 4,
-	views: views,
-	top: 0,
-	width: 200,
-	height: 112
+var view = Ti.UI.createLabel({
+    text: 'Hello World',
+    backgroundColor: 'red',
+   // height:100,
+    //left: '50%',
+    //right: '10'
+    //height: 100
+    top:30,
+    bottom: 30
+    //top: 10,
+    //bottom: 10
+   // width:100,
+   // height:100,
+    //width: Ti.UI.SIZE,
+    //height: Ti.UI.SIZE
+   // width: Ti.UI.FILL,
+    //height: Ti.UI.FILL
 });
-
-var scrollToAnimated = Ti.UI.createButton({
-	title: 'scroll animated',
-	bottom: 40,
-	right: 0
-});
-
-var scrollToNotAnimated = Ti.UI.createButton({
-	title: 'scroll',
-	bottom: 40,
-	left: 0
-});
-
-var prev = Ti.UI.createButton({
-	bottom: 0,
-	left:0,
-	title: 'prev'
-});
-var next = Ti.UI.createButton({
-	bottom: 0,
-	right:0,
-	title: 'next'
-});
-var x = 0;
-
-prev.addEventListener('click', function(){
-	scrollableView.movePrevious();
-});
-next.addEventListener('click', function(){
-	scrollableView.moveNext();
-});
-
-scrollToAnimated.addEventListener('click', function(){
-	scrollableView.scrollToView(otherView, true);
-});
-
-scrollToNotAnimated.addEventListener('click', function(){
-	scrollableView.scrollToView(2, false);
-});
-win.add(prev);
-win.add(next);
-win.add(scrollToAnimated);
-win.add(scrollToNotAnimated);
-win.add(scrollableView);
-
+win.add(view);
 win.open();
-
