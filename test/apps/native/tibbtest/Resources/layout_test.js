0b5be47c5fdbb21d20c0b226654d95225008443d
Russ McMahon <rmcmahon@appcelerator.com>
2013-06-12 20:30:56 -0700

    fixed a couple more latout issues with margins
    Conflicts:
    	src/tibb/NativeAbstractTextControlObject.cpp
    	src/tibb/NativeControlObject.h

--- test/apps/native/tibbtest/Resources/layout_test.js
+++ test/apps/native/tibbtest/Resources/layout_test.js
@@ -4,40 +4,22 @@ var win = Ti.UI.createWindow();
 
 var view = Ti.UI.createView({
     backgroundColor: '#ccc',
-    width: Ti.UI.SIZE,
+    height: Ti.UI.SIZE,
+    width: Ti.UI.SIZE
 });
 
 var label = Ti.UI.createLabel({
     text: 'hello world',
     backgroundColor: 'red',
+    left:5, right: 5,
+    top: 5, bottom: 5,
     width: Ti.UI.SIZE,
-    left: 5, 
-    right: 5
-});
-
-view.add(label);
-win.add(view);
-win.open()
-*/
-
-/*
-var win = Ti.UI.createWindow();
-
-var view = Ti.UI.createView({
-    backgroundColor: 'blue',
     height: Ti.UI.SIZE
 });
 
-var label = Ti.UI.createLabel({
-    text: 'hello world',
-    backgroundColor: 'red',
-    top: 10,
-    bottom: 10
-});
-
 view.add(label);
 win.add(view);
-win.open()
+win.open();
 */
 
 /*
@@ -58,6 +40,52 @@ var label = Ti.UI.createLabel({
 
 view.add(label);
 win.add(view);
+win.open();
+*/
+
+/*
+ var win = Ti.UI.createWindow();
+
+var view = Ti.UI.createView({
+    backgroundColor: '#ccc',
+    width: Ti.UI.SIZE,
+});
+
+var label = Ti.UI.createLabel({
+    text: 'hello world',
+    backgroundColor: 'red',
+    width: Ti.UI.SIZE,
+    left: 5, right: 5
+});
+
+view.add(label);
+win.add(view);
+win.open()
+*/
+
+/*
+var win = Ti.UI.createWindow();
+
+var label = Ti.UI.createLabel({
+    text: 'hello world',
+    backgroundColor: 'red',
+    right: 10,
+    left: 10
+});
+win.add(label);
+win.open();
+*/
+
+/*
+var win = Ti.UI.createWindow();
+
+var label = Ti.UI.createLabel({
+    text: 'hello world',
+    backgroundColor: 'red',
+    top: 10,
+    bottom: 10
+});
+win.add(label);
 win.open()
 */
 
@@ -65,16 +93,56 @@ win.open()
 var win = Ti.UI.createWindow();
 
 var view = Ti.UI.createView({
+    backgroundColor: '#ccc',
+    height: Ti.UI.SIZE,
+    width: Ti.UI.SIZE
+});
+
+var label = Ti.UI.createLabel({
+    text: 'hello world',
+    backgroundColor: 'red',
+    left:5, right: 5,
+    top: 5, bottom: 5
+});
+
+view.add(label);
+win.add(view);
+win.open();
+*/
+
+
+/*
+var win = Ti.UI.createWindow();
+
+var view = Ti.UI.createView({
 	backgroundColor: 'blue',
-	width: Ti.UI.SIZE,
-	height: Ti.UI.SIZE
+	//height: Ti.UI.SIZE
 });
 
 var label = Ti.UI.createLabel({
 	text: 'hello world',
 	backgroundColor: 'red',
-	width: Ti.UI.SIZE,
-	height: Ti.UI.SIZE
+	//height: Ti.UI.SIZE
+});
+
+view.add(label);
+win.add(view);
+win.open()
+*/
+
+/*
+var win = Ti.UI.createWindow();
+
+var view = Ti.UI.createView({
+    backgroundColor: 'blue',
+    height: Ti.UI.SIZE
+});
+
+var label = Ti.UI.createLabel({
+    backgroundColor: 'red',
+    height: 30, text: 'Hello World',
+    left: 10, right: 10,
+    top: 10, bottom: 10
 });
 
 view.add(label);
@@ -91,11 +159,10 @@ var view = Ti.UI.createView({
     height: 200
 });
 
-
 var label = Ti.UI.createLabel({
     top: 50,
 	backgroundColor: 'red',
-    text: 'kkkkkkkk',
+    text: 'hello world',
     width: Ti.UI.SIZE,
     height: Ti.UI.SIZE
 });
@@ -104,16 +171,3 @@ view.add(label);
 win.add(view);
 win.open();
 */
-
-var win = Ti.UI.createWindow({});
- 
-var view = Ti.UI.createView({
-    backgroundColor: 'green',
-    top: 10,
-    bottom: 10,
-    right: 10,
-    left: 10
-});
-
-win.add(view);
-win.open();
