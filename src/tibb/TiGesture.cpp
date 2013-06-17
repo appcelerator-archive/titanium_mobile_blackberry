14aba9f645fbd29d1e2abbb15528125960ab0a0d
Pedro Enrique <pedro.tma@gmail.com>
2013-06-17 15:03:03 -0700

    [TIMOB-14011] Added isLandscape, isPortrait, isFaceDown, and isFaceUp to Ti.Gesture

--- src/tibb/TiGesture.cpp
+++ src/tibb/TiGesture.cpp
@@ -5,9 +5,14 @@
  * Please see the LICENSE included with this distribution for details.
  */
 
-#include "TiGesture.h"
+#include <bb/device/DeviceInfo>
+#include <bb/device/DeviceOrientation>
 
+#include "TiGesture.h"
+#include "TiGenericFunctionObject.h"
 #include "NativeGestureObject.h"
+
+#include <iostream>
 
 const static TiProperty g_tiProperties[] = {
     { "orientation", TI_PROP_PERMISSION_READ, N_GESTURE_PROP_ORIENTATION }
@@ -16,6 +21,10 @@ const static TiProperty g_tiProperties[] = {
 TiGesture::TiGesture()
   : TiProxy("Gesture") {
 }
+
+
+
+
 
 void TiGesture::addObjectToParent(TiObject* parent, NativeObjectFactory* factory) {
     TiGesture* gesture = new TiGesture();
@@ -27,6 +36,10 @@ void TiGesture::addObjectToParent(TiObject* parent, NativeObjectFactory* factory
 void TiGesture::onCreateStaticMembers() {
     TiProxy::onCreateStaticMembers();
     setTiMappingProperties(g_tiProperties, sizeof(g_tiProperties) / sizeof(*g_tiProperties));
+    TiGenericFunctionObject::addGenericFunctionToParent(this, "isLandscape", this, _isLandscape);
+    TiGenericFunctionObject::addGenericFunctionToParent(this, "isPortrait", this, _isPortrait);
+    TiGenericFunctionObject::addGenericFunctionToParent(this, "isFaceUp", this, _isFaceUp);
+    TiGenericFunctionObject::addGenericFunctionToParent(this, "isFaceDown", this, _isFaceDown);
 }
 
 void TiGesture::initializeTiObject(TiObject* parentContext) {
@@ -39,4 +52,53 @@ void TiGesture::initializeTiObject(TiObject* parentContext) {
     setNativeObject(obj);
     obj->release();
 }
+
+Handle<Value> TiGesture::_isLandscape(void* userContext, TiObject* caller, const Arguments& args)
+{
+	bb::device::DeviceInfo info;
+    bb::device::DeviceOrientation::Type orientation = info.orientation();
+
+    if(orientation == bb::device::DeviceOrientation::LeftUp || orientation == bb::device::DeviceOrientation::RightUp)
+    {
+    	return Boolean::New(true);
+    }
+    return Boolean::New(false);
+}
+
+Handle<Value> TiGesture::_isPortrait(void* userContext, TiObject* caller, const Arguments& args)
+{
+	bb::device::DeviceInfo info;
+    bb::device::DeviceOrientation::Type orientation = info.orientation();
+    if(orientation == bb::device::DeviceOrientation::TopUp || orientation == bb::device::DeviceOrientation::BottomUp)
+    {
+    	return Boolean::New(true);
+    }
+    return Boolean::New(false);
+}
+
+Handle<Value> TiGesture::_isFaceDown(void* userContext, TiObject* caller, const Arguments& args)
+{
+	bb::device::DeviceInfo info;
+    bb::device::DeviceOrientation::Type orientation = info.orientation();
+
+    if(orientation == bb::device::DeviceOrientation::FaceDown)
+    {
+    	return Boolean::New(true);
+    }
+    return Boolean::New(false);
+}
+
+
+Handle<Value> TiGesture::_isFaceUp(void* userContext, TiObject* caller, const Arguments& args)
+{
+	bb::device::DeviceInfo info;
+    bb::device::DeviceOrientation::Type orientation = info.orientation();
+
+    if(orientation == bb::device::DeviceOrientation::FaceUp)
+    {
+    	return Boolean::New(true);
+    }
+    return Boolean::New(false);
+}
+
 
